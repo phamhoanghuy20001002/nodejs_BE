cf16a982a314250be6f47d6a64ca0fb17eddae86
phamhoanghuy20001002 <huy.pho.60cntt@ntu.edu.vn>
2023-03-25 10:09:01 +0700

    video19

---
+++ src/server.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const path = require('path')
+require('dotenv').config()
+const app = express()
+const port = process.env.PORT || 8888
+const hostname = process.env.HOSTNAME
+//congif template engine
+app.set('views', path.join(__dirname, 'views/'))
+app.set('view engine', 'ejs')
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+app.get('/abc', (req, res) => {
+    //res.send('router abc hieen thi')
+    res.render('sample.ejs')
+})
+
+app.listen(port, hostname, () => {
+    console.log(`Example app listening on port ${port}`)
+})
